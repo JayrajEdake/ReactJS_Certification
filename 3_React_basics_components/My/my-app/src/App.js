033d8f2f2c678247a6d592e4bf09732c75e0abf4
JayrajEdake <jayraj2910@gmail.com>
2024-08-15 20:50:00 +0530

    commit 3

--- 3_React_basics_components/My/my-app/src/App.js
+++ 3_React_basics_components/My/my-app/src/App.js
@@ -6,14 +6,14 @@ import Expenses from "./Components/Expenses/Expenses";
 const DUMMY_EXPENSES = [
   {
     id: "e1",
-    title: "Toilet Paper",
+    title: "Toilet Paper2",
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: "e3",
-    title: "Bike Insurance",
+    title: "Bike Insurance2",
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
